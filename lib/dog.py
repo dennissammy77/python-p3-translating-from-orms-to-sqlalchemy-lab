5ac0995db16ef2d3bee9449c2188e2a2e31c9ede
dennissammy77 <dennissammy77@gmail.com>
2025-05-27 13:09:16 +0300

    I am learning ORMS

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,32 @@
 from models import Dog
-
-def create_table(base):
-    pass
+from sqlalchemy import (
+    create_engine
+)
+def create_table(base,engine):
+    engine = create_engine('sqlite:///dogs.db')
+    base.metadata.create_all(engine)
 
 def save(session, dog):
+    if isinstance(dog, Dog):
+        session.add(dog)
+        session.commit()
     pass
 
 def get_all(session):
-    pass
+    return session.query(Dog)
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name.like(name)).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name.like(name),Dog.breed.like(breed)).first()
 
 def update_breed(session, dog, breed):
-    pass
+    for d in session.query(Dog).filter(Dog.name.like(dog.name)):
+        print(d)
+        d.breed = breed
+    
+    session.commit()
